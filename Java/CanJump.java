9ea8a23dacc7cee70c6164918dd93b407845c1bc
ishuprabhakar <ishuprabhakar3679@outlook.com>
2022-05-07 22:34:39 +0530

    Best Optimized solution for this problem.

--- Java/CanJump.java
+++ Java/CanJump.java
@@ -1,36 +1,33 @@
-pakage Java;
+package Java;
 
-
-class CanJump {
+/**
+ * You are given an integer array nums. You are initially positioned at the
+ * array's first index, and each element in the array represents your maximum
+ * jump length at that position.
+ * 
+ * Return true if you can reach the last index, or false otherwise.
+ * 
+ * Example 1:
+ * Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ * 
+ * Time Complexity = O(n)
+ */
+public class CanJump {
 
     public boolean canJump(int[] nums) {
-
-        boolean isJumpPossible = false;
-
-        
-
-        int index = 0;
-
-        int lastIndex = nums.length - 1;
-
-        while(index == lastIndex || nums[index] == 0){
-
-            int valueAt = nums[index];
-
-            index = valueAt;
-
-            if(valueAt == nums[lastIndex]){
-
-                isJumpPossible = true;
-
-                break;
-
+        int last = nums[nums.length - 1];
+        for (int i = nums.length - 1; i > -1; i--) {
+            if (i + nums[i] >= last) {
+                last = i;
             }
-
         }
-
-        return isJumpPossible;
-
+        if (last == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
