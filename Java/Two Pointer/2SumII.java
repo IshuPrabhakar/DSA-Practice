5de77bcc530c09afb4d9afd82a6461d724286f29
Ishu Prabhakar <72759521+IshuPrabhakar@users.noreply.github.com>
2022-07-11 21:02:40 +0530

    easy level

---
+++ Java/Two Pointer/2SumII.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int[] res = new int[2];
+        
+        int l = 0, r = numbers.length-1;
+        
+        while(l <= r){
+            
+            if(numbers[l] + numbers[r] > target){
+                r--;
+            }
+            else if(numbers[l] + numbers[r] < target){
+                l++;
+            }
+            else{
+                res[0] = l+1;
+                res[1] = r+1;
+                break;
+            }
+        }
+        
+        return res;
+    }
+}
