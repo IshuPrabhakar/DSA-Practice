b679aced41de290bd6ac116c8924438cb141e43a
Ishu Prabhakar <72759521+IshuPrabhakar@users.noreply.github.com>
2022-07-13 14:39:04 +0530

    Easy

---
+++ Java/Sliding Window/ContainsDuplicate.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+       // Use hash like Structures to maintain the elements of windows
+
+        HashSet<Integer> set = new HashSet<>();
+
+
+
+        for(int i = 0; i < nums.length; i++) {
+
+            
+
+            if(i > k)
+
+                set.remove(nums[i-k-1]);
+
+            
+
+            if(!set.add(nums[i]))
+
+                return true;
+
+        }
+
+        
+
+        return false;
+
+    }
+
+}
