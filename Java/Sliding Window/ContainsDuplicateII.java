b1f300d185b9b6486072ea2fb55d899b15f70a8c
Ishu Prabhakar <72759521+IshuPrabhakar@users.noreply.github.com>
2022-07-14 12:57:37 +0530

    Easy

--- Java/Sliding Window/ContainsDuplicateII.java
+++ Java/Sliding Window/ContainsDuplicateII.java
@@ -5,26 +5,15 @@ class Solution {
        // Use hash like Structures to maintain the elements of windows
 
         HashSet<Integer> set = new HashSet<>();
-
-
-
+        
         for(int i = 0; i < nums.length; i++) {
 
-            
-
             if(i > k)
-
                 set.remove(nums[i-k-1]);
-
             
-
             if(!set.add(nums[i]))
-
                 return true;
-
         }
-
-        
 
         return false;
 
