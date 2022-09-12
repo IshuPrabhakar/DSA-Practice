d5de261a7a5c7dc8db4b764fe92029650754d237
Ishu Prabhakar <72759521+IshuPrabhakar@users.noreply.github.com>
2022-09-12 23:06:30 +0530

    Easy - Optimal Solution apart from this exist

---
+++ Java/Tree/Two Sum IV - Input is a BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * TC => O(n)
+ * SC => O(n)
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        return dfs(root, set, k);
+    }
+    
+    private boolean dfs(TreeNode root, HashSet<Integer> set, int k) {
+        if(root == null)
+            return false;
+        
+        if(set.contains(k - root.val))
+            return true;
+        
+        set.add(root.val);
+        
+        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
+}
